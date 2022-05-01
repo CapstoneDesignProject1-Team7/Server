f95c1bfcd4518a4ebf9f76c1aac067147f14157c
ByeongGyu Jeon <ntnegm@gmail.com>
2022-05-01 14:51:38 +0900

    change select sql

--- schoolzone/src/main/java/com/schoolzone/mapper/UserDataMapper.java
+++ schoolzone/src/main/java/com/schoolzone/mapper/UserDataMapper.java
@@ -1,5 +1,7 @@
 package com.schoolzone.mapper;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -7,13 +9,13 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
-import com.schoolzone.model.UserData;
+import com.schoolzone.model.LocationData;
 
 @Mapper
 public interface UserDataMapper {
 	
-	@Select("SELECT * FROM UserData WHERE id=#{id}")
-	UserData getUserData(@Param("id") String id);
+	@Select("SELECT latitude, longitude, (6371 * acos (cos ( radians(latitude) )* cos( radians(#{latitude}) )* cos( radians(longitude) - radians(#{longitude}) )+ sin ( radians(latitude) )* sin( RADIANS(#{latitude}) ))) AS distance FROM userData HAVING distance < 0.05 ORDER BY distance;")
+	ArrayList<LocationData> getUserData(@Param("latitude") double latitude, @Param("longitude") double longitude, @Param("type") int type);
 	
 	@Insert("INSERT INTO UserData VALUES(#{id}, #{latitude}, #{longitude}, #{type})")
 	int insertUserData(@Param("id") String id, @Param("latitude") double latitude, @Param("longitude") double longitude, @Param("type") int type);
